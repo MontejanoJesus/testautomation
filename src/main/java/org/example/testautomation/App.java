92bd396ac4ee7fd8ce4a8db21d9f7eff2fcfd0c4
Jesus <91921835+jesusmontejano1@users.noreply.github.com>
2023-07-18 08:01:42 -0700

    Initial

---
+++ src/main/java/org/example/testautomation/App.java
@@ -0,0 +1,4 @@
+package org.example.testautomation;
+
+public class App {
+}
---
+++ src/main/java/org/example/testautomation/api/GetUserMethods.java
@@ -0,0 +1,19 @@
+package org.example.testautomation.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.RequestTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.Configuration;
+
+@Endpoint(url = "", methodType = HttpMethodType.POST)
+@RequestTemplatePath(path="api/linkedin/atrq.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetUserMethods extends AbstractApiMethodV2 {
+
+    public GetUserMethods() {
+        replaceUrlPlaceholder("auth_code", Configuration.getEnvArg("api_access_token") );
+    }
+}
